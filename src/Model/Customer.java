47332b5643443176f6ef2d6c90c0e12d8c4353c6
I3ali <71490447+I3ali@users.noreply.github.com>
2020-12-04 16:02:59 +0100

    Costumer and CostumerContainer updated

--- src/Model/Customer.java
+++ src/Model/Customer.java
@@ -1,7 +1,85 @@
 package Model;
+import java.util.Locale;
 
 public class Customer {
-	String a;
-	int b;
-	private String not;
+	private int ID;
+    private String group;
+    private String email;
+    private String phone;
+    private String address;
+    private String city;
+    private int zipCode;
+    public Customer (int ID, String group, String email,
+                     String phone, String address, String city, int zipCode) {
+        this.ID = ID;
+        this.group = group;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+        this.city = city;
+        this.zipCode = zipCode;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipcode(int zipcode) {
+        this.zipCode = zipcode;
+    }
+
+    public void print(){
+        System.out.println("Address: " + getAddress());
+        System.out.println("Postal Code: " + getZipCode());
+        System.out.println("City: " + getCity());
+        System.out.println("Phone number: " + getPhone());
+    }
 }
--- src/Model/CustomerContainer.java
+++ src/Model/CustomerContainer.java
@@ -1,5 +1,76 @@
 package Model;
 
-public class CustomerContainer {
+import java.util.HashMap;
+import java.util.Scanner;
 
+public class CustomerContainer {
+	private static CustomerContainer customerContainer;
+    private HashMap<String, Customer> map;
+
+    private CustomerContainer() {
+        map = new HashMap<>();
+    }
+
+    public void addCustomer() {
+        Scanner keyb = new Scanner(System.in);
+        String name;
+        int ID;
+        String group;
+        String email;
+        String phone;
+        String address;
+        String city;
+        int zipCode;
+
+        System.out.print("Enter name: ");
+        name=keyb.nextLine();
+        System.out.print("Enter ID: ");
+        ID=Integer.parseInt(keyb.nextLine());
+        System.out.print("Enter group: ");
+        group=keyb.nextLine();
+        System.out.print("Enter email: ");
+        email=keyb.nextLine();
+        System.out.print("Enter phone number: ");
+        phone=keyb.nextLine();
+        System.out.print("Enter address: ");
+        address=keyb.nextLine();
+        System.out.print("Enter city: ");
+        city=keyb.nextLine();
+        System.out.print("Enter zip code: ");
+        zipCode=Integer.parseInt(keyb.nextLine());
+        map.put(name, new Customer(ID, group, email, phone, address, city, zipCode));
+        System.out.println(name + " added to the database!\n");
+    }
+
+    public static CustomerContainer instance() {
+        if(customerContainer == null) {
+            customerContainer = new CustomerContainer();
+        }
+
+        return customerContainer;
+    }
+
+    public Boolean containsCustomer(String name){
+        return map.containsKey(name);
+    }
+
+    public Customer findCustomer(String name){
+        return map.get(name);
+    }
+
+    public void searchForCustomer(String name) {
+        Customer customer;
+        customer=map.get(name);
+
+        if (customer==null) {
+            System.out.println("\nError: Cannot find name!\n");
+        }
+        else {
+            customer.print();
+        }
+    }
+
+    public void removeCustomer(String name) {
+        map.remove(name);
+    }
 }
