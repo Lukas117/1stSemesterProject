1d92dcd15826cea40b855640b39c1a99362ee013
I3ali <71490447+I3ali@users.noreply.github.com>
2020-12-04 16:11:38 +0100

    finished 'view', but it was TUI in the previous as a note :D

--- src/View/MainMenu.java
+++ src/View/MainMenu.java
@@ -1,5 +1,54 @@
 package View;
 
-public class MainMenu {
+import java.util.Scanner;
 
+public class MainMenu {
+	public void start() {
+        mainMenu();
+    }
+
+    private void mainMenu() {
+        boolean running = true;
+        while (running) {
+            int choice = writeMainMenu();
+            switch (choice) {
+                case 1:
+                    //borrowerMenu.start();
+                    break;
+                case 2:
+                    //lpMenu.start();
+                    break;
+                case 3:
+                    //loanMenu.start();
+                    break;
+                case 4:
+                    //copyMenu.start();
+                    break;
+                case 0:
+                    System.out.println("Goodbye.");
+                    running = false;
+                    break;
+                default:
+                    System.out.println("Unknown error, choice = "+choice);
+                    break;
+            }
+        }
+    }
+    private int writeMainMenu() {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("****** Main menu ******");
+        System.out.println(" (1) Borrower menu");
+        System.out.println(" (2) LP menu");
+        System.out.println(" (3) Loan menu");
+        System.out.println(" (4) Copy menu");
+        System.out.println(" (0) Quit the program");
+        System.out.print("\n Choose:");
+
+        while (!keyboard.hasNextInt()) {
+            System.out.println("Input must be a number - try again");
+            keyboard.nextLine();
+        }
+        int choice = keyboard.nextInt();
+        return choice;
+    }
 }
