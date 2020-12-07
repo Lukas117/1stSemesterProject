8694b37d3278ea171ab41611c4c3d5a1faf81d11
I3ali <71490447+I3ali@users.noreply.github.com>
2020-12-07 18:36:34 +0100

    re-did the department class, because the names were different than we we updated in the design class diagram. Now it is the same :)

--- src/Model/Department.java
+++ src/Model/Department.java
@@ -2,26 +2,48 @@ package Model;
 
 public class Department {
 	private String name;
-    private String address;
-    
-    public Department(String name, String address) {
-        this.name = name;
-        this.address = address;
-    }
+	private String location;
+	private String type;
+	private double price;
+	
+	public Department (String name, String location, String type, double price) {
+		this.name = name;
+		this.location = location;
+		this.type = type;
+		this.price = price;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public String getAddress() {
-        return address;
-    }
+	public String getLocation() {
+		return location;
+	}
 
-    public void setAddress(String address) {
-        this.address = address;
-    }
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	
 }
