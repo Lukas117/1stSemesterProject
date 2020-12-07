6b5a7a3c4f0c4631d05613ef63c1a0a41cb3fe6b
I3ali <71490447+I3ali@users.noreply.github.com>
2020-12-07 18:32:08 +0100

    Location class getters and setters set up

---
+++ src/Model/Location.java
@@ -0,0 +1,50 @@
+package Model;
+
+public class Location {
+	
+	private String name;
+	private String location;
+	private String type;
+	private double price;
+	
+	public Location (String name, String location, String type, double price) {
+		this.name = name;
+		this.location = location;
+		this.type = type;
+		this.price = price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+
+}
