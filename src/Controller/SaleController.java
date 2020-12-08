2d6adb07f16cca376c9905b6640ac4626946bcd7
I3ali <71490447+I3ali@users.noreply.github.com>
2020-12-08 10:50:20 +0100

    finished sale with instance and the 4 function fun

--- src/Controller/SaleController.java
+++ src/Controller/SaleController.java
@@ -6,13 +6,29 @@ import Model.SaleContainer;
 import Model.Sale;
 
 public class SaleController {
-
 	private SaleContainer saleContainer;
 	
 	public SaleController() {
 		saleContainer = new SaleContainer();
 	}
+	
 	public SaleContainer getSaleContainer() {
 		return saleContainer;
+	}
+	
+	public boolean createSale(Sale sale) {
+		return saleContainer.addSale(sale);
+	}
+	
+	public Sale findSale(int ID) {
+		return saleContainer.findSale(ID);
+	}
+	
+	public Sale updateSale(int ID) {
+		return saleContainer.updateSale(ID);
+	}
+	
+	public boolean deleteEmplyee(Sale sale) {
+		return saleContainer.deleteSale(sale);
 	}
 }
