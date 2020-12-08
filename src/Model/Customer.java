79507149ed35be5b25f37447f7fdebdc88ac47f3
Lukas Kollar <lukykoll@gmail.com>
2020-12-08 10:37:52 +0100

    customer

--- src/Model/Customer.java
+++ src/Model/Customer.java
@@ -1,9 +1,8 @@
 package Model;
-import java.util.Locale;
 
 public class Customer {
-	private int ID;
-    private String group;
+	private int Id;
+    //private String group;
     private String email;
     private String phoneNumber;
     private String address;
@@ -11,79 +10,64 @@ public class Customer {
     private int zipCode;
     private String name;
     
-    public Customer (int ID, String group, String email, String phoneNumber, String address, String city, int zipCode, String name) {
-        this.ID = ID;
-        this.group = group;
-        this.email = email;
-        this.phoneNumber = phoneNumber;
-        this.address = address;
-        this.city = city;
-        this.zipCode = zipCode;
-        this.name = name;
+    public Customer (int Id, String name, String email, String phoneNumber, String address, String city, int zipCode) {
+        this.setId(Id);
+        this.setName(name);
+        //this.group = group;
+        this.setEmail(email);
+        this.setPhoneNumber(phoneNumber);
+        this.setAddress(address);
+        this.setCity(city);
+        this.setZipCode(zipCode);
     }
 
-	public int getID() {
-		return ID;
+	public int getId() {
+		return Id;
 	}
-
-	public void setID(int iD) {
-		ID = iD;
-	}
-
-	public String getGroup() {
-		return group;
-	}
-
-	public void setGroup(String group) {
-		this.group = group;
-	}
-
+//	public String getGroup() {
+//		return group;
+//	}
 	public String getEmail() {
 		return email;
 	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
 	public String getPhoneNumber() {
 		return phoneNumber;
 	}
-
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
-
 	public String getAddress() {
 		return address;
 	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
 	public String getCity() {
 		return city;
 	}
-
-	public void setCity(String city) {
-		this.city = city;
-	}
-
 	public int getZipCode() {
 		return zipCode;
 	}
-
-	public void setZipCode(int zipCode) {
-		this.zipCode = zipCode;
-	}
-
 	public String getName() {
 		return name;
 	}
-
+	
+	public void setId(int Id) {
+		this.Id = Id;
+	}
+//	public void setGroup(String group) {
+//		this.group = group;
+//	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public void setZipCode(int zipCode) {
+		this.zipCode = zipCode;
+	}
 	public void setName(String name) {
 		this.name = name;
 	}
-    
 }
--- src/Model/CustomerContainer.java
+++ src/Model/CustomerContainer.java
@@ -1,76 +1,49 @@
 package Model;
 
 import java.util.HashMap;
-import java.util.Scanner;
+import java.util.ArrayList;
+
 
 public class CustomerContainer {
-	private static CustomerContainer customerContainer;
-    private HashMap<String, Customer> map;
+	private static CustomerContainer instance;
+	private ArrayList<Customer> customerList = new ArrayList<>();
+    //private HashMap<String, Customer> map;
 
     private CustomerContainer() {
-        map = new HashMap<>();
+    	//instance = new CustomerContainer();
     }
 
-    public void addCustomer() {
-        Scanner keyb = new Scanner(System.in);
-        String name;
-        int ID;
-        String group;
-        String email;
-        String phone;
-        String address;
-        String city;
-        int zipCode;
-
-        System.out.print("Enter name: ");
-        name=keyb.nextLine();
-        System.out.print("Enter ID: ");
-        ID=Integer.parseInt(keyb.nextLine());
-        System.out.print("Enter group: ");
-        group=keyb.nextLine();
-        System.out.print("Enter email: ");
-        email=keyb.nextLine();
-        System.out.print("Enter phone number: ");
-        phone=keyb.nextLine();
-        System.out.print("Enter address: ");
-        address=keyb.nextLine();
-        System.out.print("Enter city: ");
-        city=keyb.nextLine();
-        System.out.print("Enter zip code: ");
-        zipCode=Integer.parseInt(keyb.nextLine());
-        map.put(name, new Customer(ID, group, email, phone, address, city, zipCode));
-        System.out.println(name + " added to the database!\n");
-    }
-
-    public static CustomerContainer instance() {
-        if(customerContainer == null) {
-            customerContainer = new CustomerContainer();
+    public static CustomerContainer getInstance() {
+        if(instance == null) {
+            instance = new CustomerContainer();
         }
-
-        return customerContainer;
+        return instance;
     }
 
-    public Boolean containsCustomer(String name){
-        return map.containsKey(name);
+    public boolean addCustomer(Customer newCustomer) {
+		boolean foundCustomer = false;
+		
+		if (customerList.isEmpty()) {
+        	foundCustomer = false;
+        }
+        for (Customer _customer: customerList) {
+            if (_customer.getName().equals(newCustomer.getName())) {
+            	foundCustomer = true;
+            }
+        }
+        if (!foundCustomer) {
+        	customerList.add(newCustomer);
+        }
+		return foundCustomer; 
     }
 
     public Customer findCustomer(String name){
-        return map.get(name);
+
     }
 
-    public void searchForCustomer(String name) {
-        Customer customer;
-        customer=map.get(name);
+    //public Customer
 
-        if (customer==null) {
-            System.out.println("\nError: Cannot find name!\n");
-        }
-        else {
-            //customer.print();
-        }
-    }
-
-    public void removeCustomer(String name) {
-        map.remove(name);
+    public void deleteCustomer(String name) {
+        customers.remove(name);
     }
 }
