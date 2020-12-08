4cf8945be5341464cd59fccada47f8013d4a6248
I3ali <71490447+I3ali@users.noreply.github.com>
2020-12-08 10:32:58 +0100

    saleContainer is done

--- src/Model/SaleContainer.java
+++ src/Model/SaleContainer.java
@@ -12,4 +12,59 @@ public class SaleContainer {
 	public ArrayList<Sale> getSales() {
 		return saleList;
 	}
+	
+	/* public SaleContainer getInstance(){
+	 * return instance;
+	 * }
+	 */
+	
+	public boolean addSale(Sale newSale) {
+		boolean foundSale = false;
+		
+		if (saleList.isEmpty()) {
+			foundSale = false;
+		}
+		for (Sale _sale: saleList) {
+			if (_sale.getID()==(newSale.getID())) {
+				foundSale = true;
+			}
+		}
+		if (foundSale) {
+			saleList.add(newSale);
+		}
+		return foundSale;
+	}
+	
+	public Sale findSale(int ID) {
+		for (Sale _sale : saleList) {
+			if (_sale.getID()==(ID)) {
+				return _sale;
+			}
+		}
+		return null;
+	}
+	
+	public Sale updateSale(int ID) {
+		for (Sale _sale :saleList) {
+			if (_sale.getID()==(ID)) {
+				return _sale;
+			}
+		}
+		return null;
+	}
+	
+	public boolean deleteSale(Sale sale) {
+		boolean deletedSale = false;
+		Sale saleToDelete = null;
+		for (Sale _sale: saleList) {
+			if (_sale.getID()==(sale.getID())) {
+				saleToDelete = _sale;
+				deletedSale = true;
+			}
+		}
+		if (deletedSale) {
+			saleList.remove(saleToDelete);
+		}
+		return deletedSale;
+	}
 }
