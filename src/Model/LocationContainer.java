0700b436fc421c99e1f28a03a0276ec2d69cbe1c
I3ali <71490447+I3ali@users.noreply.github.com>
2020-12-10 14:40:22 +0100

    Location Container

--- src/Model/LocationContainer.java
+++ src/Model/LocationContainer.java
@@ -11,4 +11,55 @@ public class LocationContainer {
 		return locationList;
 	}
 	
+	public static LocationContainer getInstance() {
+		if(instance == null) {
+			instance = new LocationContainer();
+		}
+		return instance;
+	}
+	
+	public boolean addLocation(Location newLocation) {
+		boolean foundLocation = false;
+		
+		if (locationList.isEmpty()) {
+			foundLocation = true;
+		}
+		for (Location location : locationList) {
+			if (location.getLocation().equals(newLocation.getLocation())) {
+				foundLocation =true;
+			}
+		}
+		if (!foundLocation) {
+			locationList.add(newLocation);
+		}
+		return foundLocation;
+	}
+	
+	public Location findLocation(int aisle) {
+		for(Location location : locationList) {
+			if(location.getAisle()==(aisle)) {
+				return location;
+			}
+		}
+		return null;
+	}
+	
+	public boolean deleteLocation(int aisle) {
+		boolean deletedLocation = false;
+		Location locationToDelete = null;
+		for(Location location1: locationList) {
+			if (location1.getAisle()==(aisle)) {
+				locationToDelete = location1;
+				deletedLocation = true;
+			}
+		}
+		if (deletedLocation) {
+			locationList.remove(locationToDelete);
+		}
+		return deletedLocation;
+	}
 }
+
+//aisle
+//department
+//
