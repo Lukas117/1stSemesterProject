4708569065f2c5da5b7fe5ff1664584c98d54f32
Marton Vadasz <75419452+VADMARCI@users.noreply.github.com>
2020-12-12 20:03:20 +0100

    .

--- src/View/LocationMenu.java
+++ src/View/LocationMenu.java
@@ -7,94 +7,104 @@ import Model.Department;
 import Model.DepartmentContainer;
 import Model.Employee;
 import Model.Location;
+import Model.LocationContainer;
 
 public class LocationMenu {
 	private LocationController locationController;
 	private Location currentLocation;
-	
+
 	public LocationMenu(LocationController locationController) {
 		this.locationController = locationController;
 	}
-	
+
 	public LocationController getLocationController() {
 		return locationController;
 	}
-	
+
 	public void start() {
 		locationMenu();
 	}
-	
+
 	private void locationMenu() {
 		boolean running = true;
-		
-		while(running) {
+
+		while (running) {
 			int choice = writeLocationMenu();
-			switch(choice) {
+			switch (choice) {
 			case 1:
-            	createLocation();
-                break;
-            case 2:
-            	deleteLocation();
-             	break;
-            case 0:
-                running = false;
-                break;
-            default:
-                System.out.println("Unknown error with choice = " + choice);
-                break;
+				createLocation();
+				break;
+			case 2:
+				deleteLocation();
+				break;
+			case 0:
+				running = false;
+				break;
+			default:
+				System.out.println("Unknown error with choice = " + choice);
+				break;
 			}
 		}
 	}
-	
-	/*
-	 * private int writeLocationMenu() { Scanner keyboard = new Scanner(System.in);
-	 * 
-	 * System.out.println("****** Location menu ******");
-	 * System.out.println(" (1) Create location");
-	 * System.out.println(" (2) Delete location");
-	 * System.out.println(" (0) Go back"); System.out.print("\n Choice: ");
-	 * 
-	 * int choice = getIntegerFromUser(keyboard); return choice; }
-	 * 
-	 * public void createLocation() { //Location location = getDataToNewLocation();
-	 * 
-	 * if (locationController.addLocation(location)) {
-	 * System.out.println("Location is already used, try different one"); } else {
-	 * locationController.addLocation(location); System.out.println("\n"); }
-	 * 
-	 * if (locationController.findLocation(aisle) !=null) {
-	 * System.out.println("----- Location -----"); } }
-	 */
-	
-	/*
-	 * public void deleteLocation() { Scanner keyboard = new Scanner(System.in);
-	 * 
-	 * System.out.
-	 * println("Type the location that you want to delete(department, aisle, shelf):"
-	 * ); String department = getStringFromUser(keyboard); int aisle =
-	 * getIntegerFromUser(keyboard); int shelf = getIntegerFromUser(keyboard);
-	 * Department department1 = null; Department department2 = null; for(Department
-	 * department3 : DepartmentContainer.getDepartmentList())
-	 * if(department1.getName().equals(department)) { department2 = department1; }
-	 * if (locationController.deleteLocation(department2, aisle, shelf)) {
-	 * System.out.println("Location deleted!\n"); }
-	 * 
-	 * else { System.out.println("Location not found.\n"); } }
-	 */
+
+	private int writeLocationMenu() { 
+		Scanner keyboard = new Scanner(System.in);
 		
-	private Integer getIntegerFromUser(Scanner keyboard) {
-    	while (!keyboard.hasNextInt()) {
-    		System.out.println("Input must be a number - try again");
-    		keyboard.nextLine();
-    	}
-    	return keyboard.nextInt();
+		System.out.println("****** Location menu ******");
+		System.out.println(" (1) Create location");
+		System.out.println(" (2) Delete location");
+		System.out.println(" (0) Go back"); System.out.print("\n Choice: ");
+		
+		int choice = getIntegerFromUser(keyboard); return choice; }
+	
+	public void createLocation() { 
+		//Location location = getDataToNewLocation();
+		  
+		if (locationController.addLocation(location)) {
+			System.out.println("Location is already used, try different one");
+		}
+		else {
+			locationController.addLocation(location); System.out.println("\n"); 
+		}
+		if (locationController.findLocation(aisle) !=null) {
+			System.out.println("----- Location -----"); 
+		}
+	}
+	 
+	public void deleteLocation() {
+		Scanner keyboard = new Scanner(System.in);
+		
+	    System.out.println("Type the location that you want to delete(department, aisle, shelf):");
+	    String department = getStringFromUser(keyboard);
+	    int aisle = getIntegerFromUser(keyboard);
+	    int shelf = getIntegerFromUser(keyboard);
+	    //Department department1 = null;
+	    //Department department2 = null;
+	    for(Department department3 : locationController.getLocationContainer().getLocationList())
+		    if(department1.getName().equals(department)) { 
+			    department2 = department1; 
+			}
+	    if (locationController.deleteLocation(department2, aisle, shelf)) {
+	    	System.out.println("Location deleted!\n");
+	    }
+	    else {
+	    	System.out.println("Location not found.\n");
+	    }
 	}
 	
-    private String getStringFromUser(Scanner keyboard) {
+	private Integer getIntegerFromUser(Scanner keyboard) {
+		while (!keyboard.hasNextInt()) {
+			System.out.println("Input must be a number - try again");
+			keyboard.nextLine();
+		}
+		return keyboard.nextInt();
+	}
+
+	private String getStringFromUser(Scanner keyboard) {
 		String inputToString = null;
-		while((inputToString = keyboard.nextLine()).isBlank()) {
-  		  System.out.println("You need to type something.");
-  		}
+		while ((inputToString = keyboard.nextLine()).isBlank()) {
+			System.out.println("You need to type something.");
+		}
 		return inputToString;
-    }
+	}
 }
